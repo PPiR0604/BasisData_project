0ab762ceb2c384c52cd77971fd0457e1d89556c6
PPiR0604 <mfahraviisk@gmail.con>
2024-11-22 23:31:49 +0700

    Init

---
+++ src/main/java/com/mycompany/project_basisdata/Project_BasisData.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_basisdata;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Project_BasisData {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
